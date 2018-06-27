5efcc0ac754c8925271580c1cb6a014bf26a8168
Jon Schulman <schulman99@gmail.com>
2018-06-27 13:41:35 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -117,5 +117,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:known_for]=["Assembly"]
+programmer_hash[:dennis_ritchie][:known_for]="Assembly"
+programmer_hash[:alan_kay][:known_for]="GUI"
 end
