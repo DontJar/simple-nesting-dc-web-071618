9839afe156a34f8cecc95ee7cca13b729956d7b3
Jon Schulman <schulman99@gmail.com>
2018-06-27 13:17:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -74,9 +74,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
 programmer_hash[:yukihiro_matsumoto] = [:known_for => "Ruby", :languages => ["LISP", "C"]]
-
 programmer_hash
 end
 
@@ -98,7 +96,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+programmer_hash[:known_for][GUI]
 
 end
 
